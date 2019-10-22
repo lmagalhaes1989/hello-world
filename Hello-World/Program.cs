e3f8daacf8f547e096a2bac243003bfaf9494594
lmagalhaes1989 <56869646+lmagalhaes1989@users.noreply.github.com>
2019-10-22 11:30:41 +0100

    Update Program.cs

--- Hello-World/Program.cs
+++ Hello-World/Program.cs
@@ -6,16 +6,24 @@ using System.Threading.Tasks;
 
 namespace Hello_World
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("New Name");
+            
+            int x = sum(10, 20);
+            
+            Console.WriteLine(x);
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+        
+        public static int sum ( int a, int b)
+        {
+            return a + b;
         }
     }
 }
